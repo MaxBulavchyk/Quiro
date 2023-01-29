6d6c9a6adcdaa9f062473ca8768acbc0f6474622
MaxBulavchyk <maxblk555@gmail.com>
2023-01-30 00:15:52 +0100

    scroll edited

--- script.js
+++ script.js
@@ -7,5 +7,10 @@ $('.owl-carousel').owlCarousel({
 
 function navToggle() {
     var navToggleNav = document.getElementById("nav");
-    navToggleNav.classList.toggle("_active")
-}
+    navToggleNav.classList.toggle("_active");
+    var html = document.getElementById("html");
+    html.classList.toggle("_overflow");
+    var body = document.getElementById("body");
+    body.classList.toggle("_overflow");
+  }
+  
